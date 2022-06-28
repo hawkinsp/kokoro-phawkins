2588b126adbf194f3754300ec150bad8611fa85c
Peter Hawkins <phawkins@google.com>
2022-06-28 03:18:36 +0000

    bt all

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -18,4 +18,4 @@ echo thread backtrace all >> t.txt
 sed -i.old 's|class FftTest(jtu.JaxTestCase):|class FftTest(jtu.JaxTestCase):\n  def setUp(self): super().setUp(); import faulthandler; faulthandler.disable()\n|' tests/fft_test.py
 #lldb --batch -s t.txt python3.7 -- tests/fft_test.py
 ulimit -c unlimited && (python3.7 tests/fft_test.py || (lldb -c `ls -t /cores/* | head -n1` \
-    --batch -o 'bt' -o 'quit' && exit 1))
+    --batch -o 'thread backtrace all' -o 'quit' && exit 1))
