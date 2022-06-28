e026810762b59a9344ab1a6c600f82add771ac5a
Peter Hawkins <phawkins@google.com>
2022-06-28 03:22:48 +0000

    newline

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -15,7 +15,7 @@ python3.7 -m pip install --user pytest
 echo run > t.txt
 echo thread backtrace all >> t.txt
 #sed -i.old 's|# Copyright 2019 Google LLC|import faulthandler; faulthandler.disable()|' tests/fft_test.py
-sed -i.old 's|class FftTest(jtu.JaxTestCase):|class FftTest(jtu.JaxTestCase):\n  def setUp(self): super().setUp(); import faulthandler; faulthandler.disable()\n|' tests/fft_test.py
+sed -i.old 's|class FftTest(jtu.JaxTestCase):|class FftTest(jtu.JaxTestCase):\\n  def setUp(self): super().setUp(); import faulthandler; faulthandler.disable()\\n|' tests/fft_test.py
 #lldb --batch -s t.txt python3.7 -- tests/fft_test.py
 ulimit -c unlimited && (python3.7 tests/fft_test.py || (lldb -c `ls -t /cores/* | head -n1` \
     --batch -o 'thread backtrace all' -o 'quit' && exit 1))
