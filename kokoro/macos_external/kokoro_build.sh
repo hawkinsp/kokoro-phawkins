b8beb21ff8fe4014f4a69c8b0f6c56a3654f5cf3
Peter Hawkins <phawkins@google.com>
2022-06-28 02:31:42 +0000

    lldb backtrace.

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -11,4 +11,6 @@ python3.7 -m pip install --user -e jax
 cd jax
 python3.7 -m pip install --user https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
 python3.7 -m pip install --user pytest
-python3.7 -m pytest tests/fft_test.py
+#python3.7 -m pytest tests/fft_test.py
+ulimit -c unlimited && (python3.7 -m pytest tests/fft_test.py || (lldb -c `ls -t /cores/* | head -n1` \
+    --batch -o 'thread backtrace all' -o 'quit' && exit 1))
