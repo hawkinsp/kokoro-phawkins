4b11930a49ff242369832479245e3316f559cace
Peter Hawkins <phawkins@google.com>
2022-06-28 02:55:44 +0000

    Disable faulthandler.

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -14,6 +14,7 @@ python3.7 -m pip install --user pytest
 #python3.7 -m pytest tests/fft_test.py
 echo run > t.txt
 echo thread backtrace all >> t.txt
+sed -i.old 's|# Copyright 2019 Google LLC|import faulthandler; faulthandler.disable()|' tests/fft_test.py
 #lldb --batch -s t.txt python3.7 -- tests/fft_test.py
 ulimit -c unlimited && (python3.7 tests/fft_test.py || (lldb -c `ls -t /cores/* | head -n1` \
     --batch -o 'thread backtrace all' -o 'quit' && exit 1))
