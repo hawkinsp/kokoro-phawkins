579367c3805994a5bffda8a275ba47ff944c977a
Peter Hawkins <phawkins@google.com>
2022-06-28 04:07:49 +0000

    fixup

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -12,10 +12,11 @@ cd jax
 git checkout faulthandler
 python3.7 -m pip install --user --upgrade pip 
 python3.7 -m pip install --user -e . 
-python3.7 -m pip install --user https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
+#python3.7 -m pip install --user https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
+python3.7 -m pip install --user https://storage.googleapis.com/jax-temp-releases/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
 python3.7 -m pip install --user pytest
 #python3.7 -m pytest tests/fft_test.py
 #sed -i.old 's|# Copyright 2019 Google LLC|import faulthandler; faulthandler.disable()|' tests/fft_test.py
 #lldb --batch -s t.txt python3.7 -- tests/fft_test.py
 ulimit -c unlimited && (python3.7 tests/fft_test.py || (lldb -c `ls -t /cores/* | head -n1` \
-    --batch -o 'settings set frame-format "frame #${frame.index}: {$frame.sp} ${frame.pc}{ ${module.file.basename}`${function.name}{${function.pc-offset}}}{ at ${line.file.basename}:${line.number}}\n' -o 'thread backtrace all' -o 'quit' && exit 1))
+    --batch -o 'thread backtrace all' -o 'quit' && exit 1))
