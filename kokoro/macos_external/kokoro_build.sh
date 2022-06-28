c11a7833bb4a34c441eee9ace7742278f722f44c
Peter Hawkins <phawkins@google.com>
2022-06-28 03:31:25 +0000

    path

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -9,7 +9,7 @@ git clone https://github.com/hawkinsp/jax.git
 cd jax
 git checkout faulthandler
 python3.7 -m pip install --user --upgrade pip 
-python3.7 -m pip install --user -e jax
+python3.7 -m pip install --user -e . 
 python3.7 -m pip install --user https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
 python3.7 -m pip install --user pytest
 #python3.7 -m pytest tests/fft_test.py
