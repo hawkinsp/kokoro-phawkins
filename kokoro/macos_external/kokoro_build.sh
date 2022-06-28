3b969e66a01dd23db06dd70a020afdef43a69e57
Peter Hawkins <phawkins@google.com>
2022-06-28 02:19:43 +0000

    Change Python version to 3.7/

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -6,8 +6,8 @@ set -x
 cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-phawkins"
 
 git clone https://github.com/google/jax.git
-python3.10 -m pip install -e jax
+python3.7 -m pip install -e jax
 cd jax
-python3.10 install https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp310-none-macosx_10_14_x86_64.whl
-python3.10 install pytest
+python3.7 install https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp37-none-macosx_10_14_x86_64.whl
+python3.7 install pytest
 pytest tests/fft_test.py
