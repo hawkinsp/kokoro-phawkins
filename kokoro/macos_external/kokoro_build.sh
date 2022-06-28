d3bc09eaa1886c081ff6f66c95a79d3f3280194a
Peter Hawkins <phawkins@google.com>
2022-06-28 02:16:59 +0000

    Add fft test.

--- kokoro/macos_external/kokoro_build.sh
+++ kokoro/macos_external/kokoro_build.sh
@@ -5,4 +5,9 @@ set -x
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/kokoro-phawkins"
 
-echo "Hello"
+git clone https://github.com/google/jax.git
+python3.10 -m pip install -e jax
+cd jax
+python3.10 install https://storage.googleapis.com/jax-releases/mac/jaxlib-0.3.14-cp310-none-macosx_10_14_x86_64.whl
+python3.10 install pytest
+pytest tests/fft_test.py
